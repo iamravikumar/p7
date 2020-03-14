6ed53e2ed4eccab8e04e8dc96c81ef2fe294cc65
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-03-14 12:47:24 +0100

    feature/api
    Repository base

---
+++ Poseidon.API/Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Poseidon.API.Repositories
+{
+    /// <summary>
+    /// Provides base repository functionality.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Poseidon.API/Repositories/RepositoryBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Poseidon.API.Data;
+
+namespace Poseidon.API.Repositories
+{
+    /// <summary>
+    /// Provides base repository functionality.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly PoseidonContext _context;
+
+        public RepositoryBase(PoseidonContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return _context
+                .Set<T>()
+                .AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return _context
+                .Set<T>()
+                .Where(expression)
+                .AsNoTracking();
+        }
+
+        public void Create(T entity)
+        {
+            _context
+                .Set<T>()
+                .Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context
+                .Set<T>()
+                .Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context
+                .Set<T>()
+                .Remove(entity);
+        }
+    }
+}
