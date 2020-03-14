469fa38772071706ca0a793c9b802957a5f4531c
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-03-14 12:24:54 +0100

    feature/api
    Refactor service configuration to extension methods

---
+++ Poseidon.API/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Poseidon.API
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureAuthorization(this IServiceCollection services)
+        {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "poseidon_api";
+                });
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo {Title = "Poseidon API", Version = "v1"});
+            });
+        }
+    }
+}
