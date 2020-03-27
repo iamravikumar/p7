eadef7106755e61f54838aaa67fe568488ac7ac5
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-03-27 10:18:48 +0100

    feature/client
    Remove old client
    Rename AuthServer > Client

---
+++ Poseidon.Client/Areas/Identity/Data/PoseidonAuthServerUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Poseidon.Client.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the PoseidonAuthServerUser class
+    public class PoseidonAuthServerUser : IdentityUser
+    {
+    }
+}
--- Poseidon.Client/Startup.cs
+++ Poseidon.Client/Startup.cs
@@ -1,75 +1,225 @@
-using System.IdentityModel.Tokens.Jwt;
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Poseidon.Client.Extensions;
+using Poseidon.Client.Areas.Identity.Data;
+using Serilog;
 
 namespace Poseidon.Client
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
+            Environment = environment;
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages(options => { options.Conventions.AuthorizePage("/Identity"); });
+            services.AddControllersWithViews();
+            services.AddMvc();
 
-            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+            services.AddLogging();
 
-            services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultScheme = "Cookies";
-                    options.DefaultChallengeScheme = "oidc";
-                })
-                .AddCookie("Cookies")
-                .AddOpenIdConnect("oidc", options =>
-                {
-                    options.Authority = "http://localhost:5000";
+            services.ConfigureIISOptions();
 
-                    options.RequireHttpsMetadata = false;
+            services.ConfigureIISServerOptions();
 
-                    options.ClientId = "poseidon_razor";
-                    options.ClientSecret = "secret";
-                    options.ResponseType = "code";
+            services.ConfigureIdentityServer(Configuration);
 
-                    options.SaveTokens = true;
-                    
-                    options.Scope.Add("poseidon_api");
-                    options.Scope.Add("offline_access");
-                });
+            services.ConfigureAuthentication();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            app.UseRouting();
+
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 condays. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseDatabaseErrorPage();
             }
 
-            app.UseHttpsRedirection();
+            InitializeDatabase(app);
+
+            CreateIdentityRoles(app, new[] { "Admin", "User" }).Wait();
+
+            CreateTestUsers(app, new[]
+            {
+                new UserSeed
+                {
+                    Email = "admin@poseidon.test",
+                    Username = "admin@poseidon.test",
+                    Password = "Pass123$",
+                    Role = "Admin"
+                },
+                new UserSeed
+                {
+                    Email = "user@poseidon.test",
+                    Username = "user@poseidon.test",
+                    Password = "Pass123$",
+                    Role = "User"
+                }
+            }).Wait();
+
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthentication();
+            app.UseIdentityServer();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
+        }
+
+        /**
+         * Internal helper methods.
+         * 
+         */
+        
+        private class UserSeed
+        {
+            public string Email { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Role { get; set; }
+        }
+        
+        private static void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.Apis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private static async Task CreateIdentityRoles(IApplicationBuilder app, IEnumerable<string> roles)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                foreach (var role in roles)
+                {
+                    var roleCheck = await roleManager.RoleExistsAsync(role);
+
+                    if (!roleCheck)
+                    {
+                        var result = await roleManager.CreateAsync(new IdentityRole(role));
+
+                        if (result.Succeeded)
+                        {
+                            Log.Debug("Role {@role} successfully created", role);
+                        }
+                        else
+                        {
+                            Log.Debug("There was a problem creating role {@role}", role);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        private static async Task CreateTestUsers(IApplicationBuilder app, IEnumerable<UserSeed> users)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var userManager =
+                    serviceScope.ServiceProvider.GetRequiredService<UserManager<PoseidonAuthServerUser>>();
+
+                foreach (var userSeed in users)
+                {
+                    var user = await userManager.FindByEmailAsync(userSeed.Email);
+
+                    if (user == null)
+                    {
+                        var newUser = new PoseidonAuthServerUser
+                        {
+                            Email = userSeed.Email,
+                            UserName = userSeed.Username,
+                            EmailConfirmed = true,
+                            PhoneNumberConfirmed = true
+                        };
+
+                        var password = new PasswordHasher<PoseidonAuthServerUser>();
+                        var hashed = password.HashPassword(newUser, userSeed.Password);
+                        newUser.PasswordHash = hashed;
+
+                        var result = await userManager.CreateAsync(newUser, userSeed.Password);
+
+                        await userManager.AddToRoleAsync(newUser, userSeed.Role);
+
+                        if (result.Succeeded)
+                        {
+                            Log.Debug("User [{@user}] successfully created", userSeed);
+                        }
+                        else
+                        {
+                            Log.Debug(
+                                "There was a problem user {@user}", userSeed);
+                        }
+                    }
+                }
+            }
         }
     }
 }
