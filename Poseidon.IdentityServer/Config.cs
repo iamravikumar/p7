3c6fff7692b78229e3d92ef20b45c96f34b1efc9
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-04-01 13:59:22 +0200

    feature/client
    Trade

--- Poseidon.IdentityServer/Config.cs
+++ Poseidon.IdentityServer/Config.cs
@@ -74,7 +74,7 @@ namespace Poseidon.Client
                 // Blazor/WASM client
                 new IdentityServer4.Models.Client
                 {
-                    AccessTokenType = AccessTokenType.Reference,
+                    // AccessTokenType = AccessTokenType.Reference,
                     ClientId = "poseidon_client",
                     ClientName = "Poseidon Client",
                     AllowedGrantTypes = GrantTypes.Implicit,
